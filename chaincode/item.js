f60871811afd7dfc1ae3a40cc2ddf3a27912ffe6
AqqleHugo <60014898+AqqleHugo@users.noreply.github.com>
2020-05-20 13:34:56 +0800

    Update item.js

--- chaincode/item.js
+++ chaincode/item.js
@@ -9,34 +9,29 @@ class Item extends Hash {
         Object.assign(this, obj);
     }
 
-
     getOwner() {
         return this.owner;
     }
-
-    setOwner(newOwner) {
-        this.owner = newOwner;
-    }
-
     getPHash(){
     	return this.provingHash;
     }
-    
     setPHash(newPHash){
     	this.provingHash = newPHash;
     }
-    static fromBuffer(buffer) {
-        return CommercialPaper.deserialize(buffer);
+    
+    //return the data get into object
+    static deserialize(data) {
+        return Hash.deserializeClass(data, Item);
     }
 
+    static fromBuffer(buffer) {
+        return Item.deserialize(buffer);
+    }
     toBuffer() {
         return Buffer.from(JSON.stringify(this));
     }
 
-    static deserialize(data) {
-        return State.deserializeClass(data, CommercialPaper);
-    }
-
+    //creation of resume item
     static createInstance(issuer, owner, issueDate, itemType, contentHash, provingHash) {
         return new Item({ issuer, owner, issueDate, itemType, contentHash, provingHash });
     }
